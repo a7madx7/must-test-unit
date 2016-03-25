eb333bcc7d7fa90f994e71d2c49a1860929104c7
a7madx7 <ahmad.hamdi.emara@gmail.com>
2016-03-25 05:01:55 +0200

    First commit

---
+++ lib/must/test/unit.rb
@@ -0,0 +1,9 @@
+require "must/test/unit/version"
+
+module Must
+  module Test
+    module Unit
+      # Your code goes here...
+    end
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'must/test/unit'
+
+require 'minitest/autorun'
